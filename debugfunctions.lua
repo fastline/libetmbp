e85a6239296be38f427b817e8a56ba740ab87723
fastline <kis.oliver@systemfarmer.hu>
2017-11-20 18:29:13 +0100

    First things firt

---
+++ debugfunctions.lua
@@ -0,0 +1,10 @@
+-- Debug Class for various helping tool
+
+local Class = require("soul/Class")
+local component = require("component")
+local side = require("sides")
+
+local Debug = Class:extend()
+function Debug:__init ()
+	Class.__init(self)
+end
